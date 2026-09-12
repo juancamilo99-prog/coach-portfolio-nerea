9372ba691ae5fcc78fcb86a991c8cf5b22d32a7e
monterocamilo99@hotmail.com <monterocamilo99@hotmail.com>
2026-09-12 19:03:42 +0200

    installation of dependecys, tailwindcss for styles and route definition

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './App.css'
+import { Home } from './pages/Home';
+import { Services } from './pages/Services';
+import { Projects } from './pages/Projects';
+import { Farmasi } from './pages/Farmasi';
+import { Contact } from './pages/Contact';
+
+
+function App() {
+
+  return (
+    <Routes>
+      <Route>
+        {/* Definimos las rutas de nuestra web las cuales dispondremos en el navbar */}
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/services" element={<Services />}></Route>
+        <Route path="/projects" element={<Projects />}></Route>
+        <Route path="/farmasi" element={<Farmasi />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+      </Route>
+    </Routes>
+  )
+}
+
+export default App
---
+++ src/pages/Farmasi.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Farmasi = () => {
+  return (
+    <div>Farmasi</div>
+  )
+}
+
+export default Farmasi
