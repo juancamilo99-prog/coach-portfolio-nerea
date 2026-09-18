724604f215ba2aa9a36205192b7f6afa8f00d07e
monterocamilo99@hotmail.com <monterocamilo99@hotmail.com>
2026-09-18 17:39:40 +0200

    feat(home) iniciando contenido estatico para el home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,42 @@ import React from 'react'
 
 export function Home() {
   return (
-    <div>Home</div>
+    <section>
+      <div>
+        {/* cinta de titulo inicio */}
+        <div>
+          <span>Coach · Autora · Emprendedora</span>
+        </div>
+        {/* mensaje titulo */}
+        <div>
+          <span>Transforma tu</span>
+          <span>vida y negocio</span>
+        </div>
+        {/* descripcion hero */}
+        <span>Soy Nerea Guisado - coach de vida, escritora, lectora y distribuidora oficial de 
+          Farmasi. Te acompaño a despertar tu potencial y crear el negocio y la vida que mereces.
+        </span>
+        {/* botones */}
+        <div>
+          <a href="">Ver Servicios</a>
+          <a href="">Mis Libros</a>
+        </div>
+        {/* clientes, libros y experiencia */}
+        <div>
+          <div>
+            <span>500+</span>
+            <span>Clientes</span>
+          </div>
+          <div>
+            <span>3</span>
+            <span>Libros</span>
+          </div>
+          <div>
+            <span>8 años</span>
+            <span>Experiencia</span>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
