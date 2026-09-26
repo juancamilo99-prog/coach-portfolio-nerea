2b5943ad83162efe48acd10c14919ef651f611b8
monterocamilo99@hotmail.com <monterocamilo99@hotmail.com>
2026-09-26 21:05:11 +0200

    feat(footer) alineamiento del grid del footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -11,16 +11,18 @@ const navItems = [
 
 export function Footer () {
   return (
-    <div className="flex flex-col items-start justify-center py-4 px-4">
+    <>
+      <div className="max-w-7xl mx-auto flex flex-col items-start justify-center py-4 px-4 md:grid md:grid-cols-3
+      md:justify-items-center">
       {/* mensaje y titulo */}
       <div>
-        <h2 className="font-display text-3xl font-bold" style={{ color: "#FF2D78" }}>Nerea</h2>
-        <span className="text-sm font-semibold">Coach de vida, autora y distribuidora oficial Farmasi. Acompañando mujeres
+        <h2 className="font-display text-3xl font-bold mb-3" style={{ color: "#FF2D78" }}>Nerea</h2>
+        <span className="text-sm leading-relaxed max-w-xs">Coach de vida, autora y distribuidora oficial Farmasi. Acompañando mujeres
           a crear la vida que merecen.
         </span>
       </div>
       { /* Navegacion */}
-      <div className="py-4">
+      <div>
         <h3 className="font-bold" style={{ color: "#FF2D78" }}>NAVEGACIÓN</h3>
         <nav className="flex flex-col gap-2 py-3 font-semibold text-sm">
           {
@@ -42,5 +44,15 @@ export function Footer () {
         <span className="text-sm font-semibold">Valencia, España</span>
       </div>
     </div>
+    <div className="border border-border w-full"></div>
+      <div className="flex flex-col items-center mx-auto py-2">
+        <p className="text-sm">
+          © 2026 Nerea Sanchez, Todos los derechos reservados.
+        </p>
+        <p className="text-sm">
+          Hecho con 💖 y muchas energias
+        </p>
+      </div>
+    </>
   )
 }
